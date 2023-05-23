4312558c90ca81cf035e50dffca39a275f08e3ad
MatteoPie <mattypierno00@gmail.com>
2023-05-23 18:47:57 +0200

    New Main Branch

--- web-service-games-repository/src/main/java/com/sad/g15/webservicegamesrepository/DataAccess/Entity/MatchHistory.java
+++ web-service-games-repository/src/main/java/com/sad/g15/webservicegamesrepository/DataAccess/Entity/MatchHistory.java
@@ -1,107 +1,91 @@
 package com.sad.g15.webservicegamesrepository.DataAccess.Entity;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
-import jakarta.persistence.*;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 
 //This Class is also named StoricoPartita in the doc Entity Class Diagram
-@Entity (name = "MatchHistory")
-@Table (name = "matchHistory")                 //unique constraint go in here
+@Entity(name = "MatchHistory")
+@Table(name = "matchHistory") // unique constraint go in here
 public class MatchHistory {
 
+	public MatchHistory(int id, String scenario, LocalDateTime startDate, LocalDateTime endDate, List<Round> rounds) {
+		super();
+		this.id = id;
+		this.scenario = scenario;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.rounds = rounds;
+	}
+
 	@Id
-    @SequenceGenerator(
-            name = "matchHistory_sequence",
-            sequenceName = "matchHistory_sequence",
-            allocationSize = 1
-    )
-	@GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "matchHistory_sequence"
-    )
-    @Column(name = "id", updatable = false)
-    private int id;
-    private String scenario;
-    private LocalDateTime startDate;
-    private LocalDateTime endDate;
-    private Boolean result;
+	@SequenceGenerator(name = "matchHistory_sequence", sequenceName = "matchHistory_sequence", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "matchHistory_sequence")
+	@Column(name = "id", updatable = false)
+	private int id;
+	private String scenario;
+	private LocalDateTime startDate;
+	private LocalDateTime endDate;
 
-    @OneToMany(orphanRemoval = true)
-    private List<Round> rounds;
+	@OneToMany(orphanRemoval = true)
+	private List<Round> rounds;
 
-    public MatchHistory(int id, String scenario, LocalDateTime startDate, LocalDateTime endDate, Boolean result, List<Round> rounds) {
-        this.id = id;
-        this.scenario = scenario;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.result = result;
-        this.rounds = new ArrayList<Round>();
-    }
+	public MatchHistory() {
 
-    public MatchHistory() {
+	}
 
-    }
+	public int getId() {
+		return id;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public String getScenario() {
+		return scenario;
+	}
 
-    public String getScenario() {
-        return scenario;
-    }
+	public void setScenario(String scenario) {
+		this.scenario = scenario;
+	}
 
-    public void setScenario(String scenario) {
-        this.scenario = scenario;
-    }
+	public LocalDateTime getStartDate() {
+		return startDate;
+	}
 
-    public LocalDateTime getStartDate() {
-        return startDate;
-    }
+	public void setStartDate(LocalDateTime startDate) {
+		this.startDate = startDate;
+	}
 
-    public void setStartDate(LocalDateTime startDate) {
-        this.startDate = startDate;
-    }
+	public LocalDateTime getEndDate() {
+		return endDate;
+	}
 
-    public LocalDateTime getEndDate() {
-        return endDate;
-    }
+	public void setEndDate(LocalDateTime endDate) {
+		this.endDate = endDate;
+	}
 
-    public void setEndDate(LocalDateTime endDate) {
-        this.endDate = endDate;
-    }
+	public List<Round> getRounds() {
+		return rounds;
+	}
 
-    public Boolean getResult() {
-        return result;
-    }
+	public void setRounds(List<Round> rounds) {
+		this.rounds = rounds;
+	}
 
-    public void setResult(Boolean result) {
-        this.result = result;
-    }
+	@Override
+	public String toString() {
+		return "MatchHistory [id=" + id + ", scenario=" + scenario + ", startDate=" + startDate + ", endDate=" + endDate
+				+ ", rounds=" + rounds + "]";
+	}
 
-    public List<Round> getRounds() {
-        return rounds;
-    }
-
-    public void setRounds(List<Round> rounds) {
-        this.rounds = rounds;
-    }
-
-    @Override
-    public String toString() {
-        return "MatchHistory{" +
-                "id=" + id +
-                ", scenario='" + scenario + '\'' +
-                ", startDate=" + startDate +
-                ", endDate=" + endDate +
-                ", result=" + result +
-                ", rounds=" + rounds +
-                '}';
-    }
 }
