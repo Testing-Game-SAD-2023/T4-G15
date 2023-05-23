8e3df54f4b5684e0dd0550c7499ebef698655d9c
MatteoPie <mattypierno00@gmail.com>
2023-05-23 11:57:10 +0200

    ResultRepository Added

--- web-service-games-repository/src/main/java/com/sad/g15/webservicegamesrepository/DataAccess/Repository/ResultRepository.java
+++ web-service-games-repository/src/main/java/com/sad/g15/webservicegamesrepository/DataAccess/Repository/ResultRepository.java
@@ -1,9 +1,10 @@
 package com.sad.g15.webservicegamesrepository.DataAccess.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.sad.g15.webservicegamesrepository.DataAccess.Entity.MatchHistory;
 
-public interface MatchHistoryRepository extends JpaRepository<MatchHistory,Integer> {
+import com.sad.g15.webservicegamesrepository.DataAccess.Entity.Result;
+
+public interface ResultRepository extends JpaRepository<Result,Integer> {
 
     //Repository of the Match History Class, every entity class has one (implement other Repos)
 
